911b27788aa02ea8e0891f7bb1dbd1279a6ba3e6
Rodrigo Franco <rodrigocsfranco@gmail.com>
2020-09-25 07:48:38 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace wcr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nota = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nome,
+            this.Nota});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(792, 337);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.MinimumWidth = 6;
+            this.Nome.Name = "Nome";
+            this.Nome.Width = 437;
+            // 
+            // Nota
+            // 
+            this.Nota.HeaderText = "Nota";
+            this.Nota.MinimumWidth = 6;
+            this.Nota.Name = "Nota";
+            this.Nota.Width = 103;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Carregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nota;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wcr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var wc = new WebClient();
+            string pagina = wc.DownloadString("https://www.metacritic.com/browse/games/release-date/new-releases/all/date");
+
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(pagina);
+
+            dataGridView1.Rows.Clear();
+
+            string id = string.Empty;
+            string nome = string.Empty;
+            string nota = string.Empty;
+            string imagem = string.Empty;
+            string link = string.Empty;
+
+
+            var dados = htmlDoc.DocumentNode.Descendants("td").Where(node => node.GetAttributeValue("class", "").Equals("clamp-summary-wrap")).ToList<HtmlNode>();
+
+            foreach (HtmlNode node in dados)
+            {
+                nome = node.Descendants("a").Where(n => n.GetAttributeValue("class", "").Equals("title")).ToList()[0].ChildNodes[0].InnerText;
+                try
+                {
+                    nota = node.Descendants("div").Where(n => n.GetAttributeValue("class", "").Equals("metascore_w large game positive")).ToList()[0].ChildNodes[0].InnerText;
+                }
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    try
+                    {
+                        nota = node.Descendants("div").Where(n => n.GetAttributeValue("class", "").Equals("metascore_w large game mixed")).ToList()[0].ChildNodes[0].InnerText;
+                    }
+                    catch (System.ArgumentOutOfRangeException)
+                    {
+                        try
+                        {
+                            nota = node.Descendants("div").Where(n => n.GetAttributeValue("class", "").Equals("metascore_w large game negative")).ToList()[0].ChildNodes[0].InnerText;
+                        }
+                        catch (System.ArgumentOutOfRangeException)
+                        {
+
+                        }
+                    }
+                }
+
+
+                if (!string.IsNullOrEmpty(nome))
+                {
+                    dataGridView1.Rows.Add(nome, nota);
+                }
+
+            }
+        }
+    }
+}
